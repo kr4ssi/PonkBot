26fdc44b0373317386f594664422038d37d7e30e
kr4ssi@tuta.io <kr4ssi@tuta.io>
2020-01-12 22:47:32 +0100

    auto add

--- balloons/ponk_autoadd.js
+++ balloons/ponk_autoadd.js
@@ -10,23 +10,29 @@ const Watcher = require('rss-watcher')
 class autoAdd {
   constructor(ponk){
     Object.assign(this, {
-      bot         : ponk   // The bot
+      ids: [
+        'UCNqljVvVXoMv9T7dPTvg0JA',
+        'UCVtVOddS0ms54kntAYXIVGg'
+      ],        // Youtube-Channel-Ids to watch
+      bot: ponk // The bot
     })
-    const watcher = new Watcher('https://www.youtube.com/feeds/videos.xml?channel_id=UCNqljVvVXoMv9T7dPTvg0JA')
-    watcher.on('new article', article => {
-      this.bot.db.getKeyValue('newfeed').then(newfeed => {
-        //console.log(newfeed, article, article.link, article.title)
-        if (article.link === newfeed) return
-        this.bot.sendMessage(article.title + ' addiert')
-        this.bot.API.add.add(article.link, undefined, {fiku: true})
-        this.bot.db.setKeyValue('newfeed', article.link)
-      })
-    }).on('error', err => {
-      console.error(err)
-    }).run((err, articles) => {
-      if (err) return console.error(err)
-      articles.forEach(article => {
-        //console.log(article)
+    this.ids.forEach(id => {
+      const watcher = new Watcher('https://www.youtube.com/feeds/videos.xml?channel_id=' + id)
+      watcher.on('new article', article => {
+        this.bot.db.getKeyValue('newfeed').then(newfeed => {
+          //console.log(newfeed, article, article.link, article.title)
+          if (article.link === newfeed) return
+          this.bot.sendMessage(article.title + ' addiert')
+          this.bot.API.add.add(article.link, undefined, {fiku: true})
+          this.bot.db.setKeyValue('newfeed', article.link)
+        })
+      }).on('error', err => {
+        console.error(err)
+      }).run((err, articles) => {
+        if (err) return console.error(err)
+        articles.forEach(article => {
+          //console.log(article)
+        })
       })
     })
   }
