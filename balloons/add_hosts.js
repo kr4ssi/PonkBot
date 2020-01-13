c33a215513577435c03b173967b399a0ecf08faa
kr4ssi@tuta.io <kr4ssi@tuta.io>
2020-01-13 06:43:26 +0100

    tvnow

--- balloons/add_hosts.js
+++ balloons/add_hosts.js
@@ -58,7 +58,7 @@ class HosterList {
                 {type: 'audio/aac', ext: ['.aac']},
                 {type: 'audio/ogg', ext: ['.ogg']},
                 {type: 'audio/mpeg', ext: ['.mp3', '.m4a']}
-              ].find(contentType => contentType.ext.includes(path.extname(URL.parse(this.fileurl).pathname))) || {}).type || 'video/mp4'
+              ].find(contentType => contentType.ext.some(ext => URL.parse(this.fileurl).pathname.endsWith(ext))) || {}).type || 'video/mp4'
             }
           ]
         }
@@ -347,6 +347,19 @@ class HosterList {
           }).then(({ match }) => {
             this.title = match[1]
             this.fileurl = 'https://' + this.matchGroup('host') + '/stream' + this.matchGroup('id') + '.mp4'
+            return this
+          })
+        },
+        type: 'cm'
+      },
+      'tvnow.de': {
+        regex: /https?:\/\/((?:www\.)?tvnow.de)\/(.*)/,
+        getInfo() {
+          return ponk.fetch(this.url, {
+            match: /<title>(.*?) im Online Stream[\s\S]+(https:\/\/vodnowusoaws[^;]*?\/\.mpd)/,
+          }).then(({ match }) => {
+            this.title = match[1]
+            this.fileurl = match[2]
             return this
           })
         },
